960a30fafeb65c9731c26350853c6d0ecc9be05c
Vinh Chu <vinhchugg@gmail.com>
2021-10-16 05:04:20 +0100

    Update header.js

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,8 +1,25 @@
 import React from "react";
 // import NavBar from "./NavBar";
-
+import "./header.css";
 function Header() {
-  return <header></header>;
+  return (
+    <header className="header">
+      <a className="name" href="#">
+        Vinh Chu
+      </a>
+      <div className="nav-container">
+        <div className="icons">
+          <a className="" href="#">
+            github
+          </a>
+          <a className="" href="#">
+            linkedin
+          </a>
+        </div>
+        <div className="menu-icon"></div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
