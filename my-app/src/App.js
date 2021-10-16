ed568d3bad07e86ee2b8cd6c4afadc1ceb94ca08
Vinh Chu <vinhchugg@gmail.com>
2021-10-16 05:05:16 +0100

    Fix app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,11 +2,10 @@ import Header from "./components/Header";
 import Main from "./components/Main";
 function App() {
   return (
-    <header className="App">
-      <div>Hello</div>
+    <>
       <Header />
       <Main />
-    </header>
+    </>
   );
 }
 
